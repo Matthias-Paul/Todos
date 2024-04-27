eadf3b5b021b537108197f47bc7efe22db6e17d5
Matthias-Paul <pauladesina117@gmail.com>
2024-04-27 14:44:16 -0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Todo from './Todo';
+import Header from './Header.jsx';
+import "./app.css";
+export default function App(){
+
+
+return(
+<>
+<Header/>
+<Todo />
+
+
+
+</>
+)
+}
---
+++ src/Todo.jsx
@@ -0,0 +1,29 @@
+import { useState, useEffect } from "react";
+import Form from "./Form";
+import TodoList from "./TodoList";
+import Footer from "./Footer";
+export default function Todo() {
+  const [todos, setTodos] = useState([]);
+  const completedTodos = todos.filter((todo) => todo.done).length;
+  const totalTodos=todos.length
+
+  useEffect (()=> {
+const storedTodos = localStorage.getItem("todos");
+if(storedTodos){
+  setTodos(JSON.parse(storedTodos))
+}
+  },[])
+
+  useEffect (()=> {
+     localStorage.getItem("todos", JSON.stringify(todos));
+
+      },[todos])
+
+  return (
+    <>
+      <Form todos={todos} setTodos={setTodos} />
+      <TodoList todos={todos} setTodos={setTodos} />
+      <Footer completedTodos={completedTodos} totalTodos={totalTodos} />
+    </>
+  );
+}
